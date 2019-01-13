74dcde1d854a0cdaadb47a9dcad4c3c2f5691d6a
Ильдар Ахметов <akhmetov.ih@yandex.ru>
2019-01-13 05:12:44 +0300

    add the fileEquals function

--- FilesTools/MainWindow.xaml.cs
+++ FilesTools/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 using WinForms = System.Windows.Forms;
 
 namespace FilesTools
@@ -65,6 +66,27 @@ namespace FilesTools
                 diffrents.Text = folderDialog.SelectedPath;
             }
 
+        }
+
+        private bool fileEquals(string file1, string file2)
+        {
+            using (FileStream s1 = new FileStream(file1, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (FileStream s2 = new FileStream(file2, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (BinaryReader b1 = new BinaryReader(s1))
+            using (BinaryReader b2 = new BinaryReader(s2))
+            {
+                while (true)
+                {
+                    byte[] data1 = b1.ReadBytes(64 * 1024);
+                    byte[] data2 = b2.ReadBytes(64 * 1024);
+                    if (data1.Length != data2.Length)
+                        return false;
+                    if (data1.Length == 0)
+                        return true;
+                    if (!data1.SequenceEqual(data2))
+                        return false;
+                }
+            }
         }
     }
 }
